fb2e92af85b02373bc77fd85d205c10c1fefb291
Barracas <azazazaaaaaa1@gmail.com>
2021-04-14 09:12:03 -0500

    Parte visual ionic

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const { spotifyRoute } = require("./routes/spotify");
 require('dotenv').config();
+const path = require('path');
 
 const corsOption = {
     origin: "*",
@@ -13,7 +14,18 @@ app.use(cors(corsOption));
 
 app.use(express.json());
 
+//public
+app.use(express.static('public'))
+
+//rutas
 app.use('/api/music', spotifyRoute)
+
+//VISTAS
+app.get('*', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+
+});
 
 app.listen( process.env.port || 8080,()=>{
     console.log("Corriendo servidor en el puerto " +  process.env.port || 8080)
